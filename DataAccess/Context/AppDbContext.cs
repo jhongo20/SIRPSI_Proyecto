3f597363f7c36983912360358e7b998a89024c07
John Leal <john.culma@outlook.com>
2023-05-30 20:35:54 -0500

    Referencias y otros

---
+++ DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,75 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Reflection.Emit;
+using DataAccess.Models.Companies;
+using DataAccess.Models.Estados;
+using DataAccess.Models.Status;
+using DataAccess.Models.Country;
+using DataAccess.Models.Documents;
+using DataAccess.Models.Users;
+using DataAccess.Models.Rols;
+
+namespace DataAccess.Context
+{
+    public partial class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+
+        public DbSet<AspNetUsers> AspNetUsers { get; set; }
+        public DbSet<Roles> AspNetRoles { get; set; }
+        public DbSet<UserRoles> AspNetUserRoles { get; set; }
+        public DbSet<Empresas> empresas { get; set; }
+        public DbSet<Estados> estados { get; set; }
+        public DbSet<TiposEmpresa> tiposEmpresas { get; set; }
+        public DbSet<Pais> pais { get; set; }
+        public DbSet<TiposDocumento> tiposDocumento { get; set; }
+
+
+        //private void SeeData(ModelBuilder modelBuilder)
+        //{
+        //    var rolAdminId = "3134ea79-fd86-4f89-9784-cbf6b6b18f82";
+        //    var userAdminId = "f1173d7f-eddd-4194-bd76-2c6244b2224c";
+        //    var userName = "john.culma@outlook.com";
+
+        //    var rolAdmin = new IdentityRole()
+        //    {
+        //        Id = rolAdminId,
+        //        Name = "Administrador",
+        //        NormalizedName = "Administrador"
+        //    };
+        //    var passwordHasher = new PasswordHasher<IdentityUser>();
+        //    var usuarioAdmin = new IdentityUser()
+        //    {
+        //        Id = userAdminId,
+        //        UserName = userName,
+        //        NormalizedUserName = userName,
+        //        Email = userName,
+        //        NormalizedEmail = userName,
+        //        PasswordHash = passwordHasher.HashPassword(null, "John.fcl14*")
+        //    };
+        //    modelBuilder.Entity<IdentityUser>()
+        //        .HasData(usuarioAdmin);
+        //    modelBuilder.Entity<IdentityRole>()
+        //        .HasData(rolAdmin);
+        //    modelBuilder.Entity<IdentityUserClaim<string>>()
+        //        .HasData(new IdentityUserClaim<string>()
+        //        {
+        //            Id = 1,
+        //            ClaimType = ClaimTypes.Role,
+        //            UserId = userAdminId,
+        //            ClaimValue = "Administrador"
+        //        });
+        //}
+    }
+}
---
+++ DataAccess/Models/Companies/Empresas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models.Status
+{
+    [Table("Empresas", Schema = "sirpsi")]
+    public partial class Empresas
+    {
+        public string Id { get; set; } = null!;
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdConsecutivo { get; set; }
+
+        public string? TipoDocumento { get; set; }
+
+        public string? DigitoVerificacion { get; set; }
+
+        public string? IdTipoEmpresa { get; set; }
+
+        public string Documento { get; set; } = null!;
+
+        public string Nombre { get; set; } = null!;
+
+        public string Descripcion { get; set; } = null!;
+
+        public string? Observacion { get; set; }
+
+        public string? IdEstado { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+
+        public string UsuarioRegistro { get; set; } = null!;
+
+        public DateTime? FechaModifico { get; set; }
+
+        public string? UsuarioModifico { get; set; }
+
+    }
+}
